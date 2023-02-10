cc7b9348e683f33c55e2d86aaf73eea69721ecbd
punchi001 <k.njehu@gmail.com>
2023-02-10 14:36:23 +0300

    mongodb setup,created connection,inserted data

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,29 @@
+require('../models/database');
+const Category = require('../models/Category');
+
 // Getting our homepage
 
 exports.homepage = async(req, res) => {
     res.render('index', { title: 'Cooking Blog - Home' });
 }
+
+
+async function insertDummyCategoryData() {
+    try {
+        const categories = [
+            { name: "Thai", image: "thai-food.jpg" },
+            { name: "American", image: "american-food.jpg" },
+            { name: "Chinese", image: "chinese-food.jpg" },
+            { name: "Mexican", image: "mexican-food.jpg" },
+            { name: "Indian", image: "indian-food.jpg" },
+            { name: "Spanish", image: "spanish-food.jpg" },
+        ];
+
+        await Category.create(categories);
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+
+insertDummyCategoryData();
