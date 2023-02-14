c7911e9278fddefc4dd2e18dda082ec6ccf1a6af
punchi001 <k.njehu@gmail.com>
2023-02-14 11:45:41 +0300

    todo: format the images to look nice

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -4,26 +4,40 @@ const Category = require('../models/Category');
 // Getting our homepage
 
 exports.homepage = async(req, res) => {
-    res.render('index', { title: 'Cooking Blog - Home' });
-}
 
-
-async function insertDummyCategoryData() {
     try {
-        const categories = [
-            { name: "Thai", image: "thai-food.jpg" },
-            { name: "American", image: "american-food.jpg" },
-            { name: "Chinese", image: "chinese-food.jpg" },
-            { name: "Mexican", image: "mexican-food.jpg" },
-            { name: "Indian", image: "indian-food.jpg" },
-            { name: "Spanish", image: "spanish-food.jpg" },
-        ];
 
-        await Category.create(categories);
+        const limitNumber = 5;
+        const categories = await Category.find({}).limit(limitNumber);
+
+        res.render('index', { title: 'Cooking Blog - Home' ,categories});
+
+        
     } catch (error) {
-        console.log('Error:', error);
+
+        res.status(500).send({message: error.message || "Error Occured "});
+        
     }
 }
 
 
-insertDummyCategoryData();
+
+// 
+
+// async function insertDummyCategoryData() {
+//     try {
+//         const categories = [
+//             { name: "Thai", image: "thai-food.jpg" },
+//             { name: "American", image: "american-food.jpg" },
+//             { name: "Chinese", image: "chinese-food.jpg" },
+//             { name: "Mexican", image: "mexican-food.jpg" },
+//             { name: "Indian", image: "indian-food.jpg" },
+//             { name: "Spanish", image: "spanish-food.jpg" },
+//         ];
+
+//         await Category.create(categories);
+//     } catch (error) {
+//         console.log('Error:', error);
+//     }
+// }
+// insertDummyCategoryData();
