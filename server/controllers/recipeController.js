41192416b168245302f673b0959661903f7b7bb2
Vivian <vivyan3gmail.com>
2023-03-31 21:12:11 +0300

    render json search  results with images & contents

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -80,16 +80,11 @@ exports.searchRecipe =async(req,res) =>{
 try {
   let searchTerm =req.body.searchTerm;
   let recipe =await Recipe.find({ $text: { $search: searchTerm,$diacriticSensitive:true}})
-res.json(recipe);
+  res.render('search',{title:'Cooking Blog- Search',recipe});;
 
 } catch (error) {
   res.status(500).send({message: error.message || "Error Occured"})
   
 }
-
-
-
-
-
-  res.render('search',{title:'Cooking Blog- Search'});
+  
 }
