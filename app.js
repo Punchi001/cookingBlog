18f870634b03a9064a683758287aa3ca43bfe730
Kenneth Njehu <k.njehu@gmail.com>
2023-04-04 17:34:27 +0300

    added some middleware for file upload securing our session

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
 
 const app = express();
 const port = process.env.PORT || 7171;
@@ -9,6 +14,15 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(expressLayouts);
+
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+    secret: 'CookingBlogSecretSession',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+app.use(fileUpload());
 
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
